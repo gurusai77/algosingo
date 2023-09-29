60cdc314550bba98bc593506a85abcbf88c783d4
Guru Sai Voleti <77335980+gurusaivoleti-discovery@users.noreply.github.com>
2023-09-29 11:42:07 +0530

    update

---
+++ linkedlist/detectcycle.go
@@ -0,0 +1,14 @@
+package main
+
+//func (l *List) DetectCycle() bool {
+//	slow := l.head
+//	fast := l.head.next
+//	for slow != nil && fast != nil && fast.next != nil {
+//		slow = slow.next
+//		fast = fast.next.next
+//		if slow == fast {
+//			return true
+//		}
+//	}
+//	return false
+//}
--- linkedlist/main.go
+++ linkedlist/main.go
@@ -70,21 +70,53 @@ func (l *List) ShowCircular() {
 }
 
 func main() {
-	sl := List{}
+	// sl := List{}
+	//for i := 0; i < 5; i++ {
+	//	sl.Insert(rand.Intn(100))
+	//}
+	//Show(&sl)
+	//sl.Insert(23)
+	//sl.Insert(24)
+	//sl.Insert(56)
+	//sl.Insert(57)
+	//fmt.Println()
+	//Show(&sl)
+	//sl.Remove(23)
+	//fmt.Println()
+	//Show(&sl)
+	//sl.ConvertToCircular()
+	//fmt.Println()
+	//sl.ShowCircular()
+
+	cycleList := List{}
 	for i := 0; i < 5; i++ {
-		sl.Insert(rand.Intn(100))
+		cycleList.Insert(rand.Intn(100))
 	}
-	Show(&sl)
-	sl.Insert(23)
-	sl.Insert(24)
-	sl.Insert(56)
-	sl.Insert(57)
+	Show(&cycleList)
 	fmt.Println()
-	Show(&sl)
-	sl.Remove(23)
+	cycleList.head.next.next.next.next.next = cycleList.head.next
+	// cycleList.ShowCircular()
 	fmt.Println()
-	Show(&sl)
-	sl.ConvertToCircular()
-	fmt.Println()
-	sl.ShowCircular()
+	n1, n2, flag := cycleList.DetectCycle()
+	if flag {
+		fmt.Println("linked list loop detected")
+		fmt.Println(n1)
+		fmt.Println(n2)
+	}
+
+}
+
+func (l *List) DetectCycle() (*Node, *Node, bool) {
+	slow := l.head
+	fast := l.head.next
+	for slow != nil && fast != nil && fast.next != nil {
+		fmt.Printf("slow node : %v\n", slow)
+		fmt.Printf("fast node : %v\n", fast)
+		if slow == fast {
+			return slow, fast, true
+		}
+		slow = slow.next
+		fast = fast.next.next
+	}
+	return nil, nil, false
 }
