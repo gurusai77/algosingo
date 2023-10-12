9e96219aada81db0daa3525e20655f88ddcaa42b
Guru Sai Voleti <77335980+gurusaivoleti-discovery@users.noreply.github.com>
2023-10-12 09:03:39 +0530

    update

---
+++ arrays/3sumproblem/main.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+// brute force, o(n3) and space complexity as we are using extra array
+func tripletsSum(a []int, sum int) (l [][]int) {
+	n := len(a)
+	for i := 0; i < n-1; i++ {
+		for j := i + 1; j < n-1; j++ {
+			for k := j + 1; k < n-1; k++ {
+				if a[i]+a[j]+a[k] == sum {
+					l = append(l, []int{a[i], a[j], a[k]})
+				}
+			}
+		}
+	}
+	return l
+}
+
+// better approach using pointers 0(n2)
+func tripSum(a []int, reqSum int) (l [][]int) {
+	n := len(a)
+	for i := 0; i < n; i++ {
+		if i != 0 && a[i] == a[i-1] {
+			continue
+		}
+		j, k := i+1, n-1
+		for j < k {
+			sum := a[i] + a[j] + a[k]
+			if sum < reqSum {
+				j++
+			} else if sum > reqSum {
+				k--
+			} else {
+				l = append(l, []int{a[i], a[j], a[k]})
+				j++
+				k--
+				for j < k && a[j] == a[j-1] {
+					j++
+				}
+				for j < k && a[k] == a[k+1] {
+					k--
+				}
+			}
+		}
+	}
+	return l
+}
+
+func main() {
+	// pass a sorted array
+	a := tripletsSum([]int{1, 2, 3, 4, 5, 6}, 6)
+	fmt.Print(a)
+	b := tripSum([]int{-4, -1, -1, 0, 1, 2}, 0)
+	fmt.Print(b)
+	b = tripSum([]int{1, 2, 2, 3, 4, 5, 6}, 6)
+	fmt.Print(b)
+}
---
+++ arrays/4sumproblem/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+// better approach using pointers o(n3)
+func fourSumProblem(a []int, reqSum int) (store [][]int) {
+	n := len(a)
+	for i := 0; i < n; i++ {
+		if i != 0 && a[i] == a[i-1] {
+			continue
+		}
+		for j := i + 1; j < n; j++ {
+			if j != 0 && a[j] == a[j-1] {
+				continue
+			}
+			k, l := j+1, n-1
+			for k < l {
+				sum := a[i] + a[j] + a[k] + a[l]
+				if sum < reqSum {
+					k++
+				} else if sum > reqSum {
+					l--
+				} else {
+					store = append(store, []int{a[i], a[j], a[k], a[l]})
+					k++
+					l--
+					for k < l && a[k] == a[k-1] {
+						j++
+					}
+					for k < l && a[l] == a[l+1] {
+						l--
+					}
+				}
+			}
+		}
+	}
+	return store
+}
+
+func main() {
+	// pass a sorted array
+	a := fourSumProblem([]int{1, 2, 3, 4, 5, 6, 7, 8}, 22)
+	fmt.Print(a)
+}
